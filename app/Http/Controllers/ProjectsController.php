567720663ced2ba0028332099115955dc780fce6
shaho1090 <33988792+shaho1090@users.noreply.github.com>
2019-11-03 11:41:12 +0330

    I do not use DB::table anymore in this file

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -7,7 +7,6 @@ use App\Project;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\ContributorsController;
 use App\UserProject;
 
@@ -15,20 +14,10 @@ use App\UserProject;
 
 class ProjectsController extends Controller
 {
-    /*
-     * I use Route-model Binding instead this method
-     */
-   /* public function index()
-    {
-        $user_id = Auth::user()->id;
-        $projects = User::find($user_id)->projects()->get();
-        return view('projects.index', ['projects' => $projects]);
-    }*/
-
     public function editProject($project_id)
     {
-        $project = DB::table('projects')
-            ->where('id',$project_id)
+        $project = new Project();
+        $project =  $project->where('id',$project_id)
             ->get()
             ->first();
         $contributors = Project::find($project_id)
@@ -46,9 +35,9 @@ class ProjectsController extends Controller
 
         $project_title = $request->get('project_title');
         $project_id = $request->get('project_id');
+        $project = new Project();
 
-        DB::table('projects')
-            ->where('id', $project_id)
+        $project = $project->where('id', $project_id)
             ->update(['title' => $project_title]);
 
         return redirect()->action(
@@ -59,21 +48,19 @@ class ProjectsController extends Controller
 
     public function createNewProject(ProjectFormRequest $request)
     {
-        $user_id = Auth::user()->id;
+        $user = new User();
+        $user_id =$user->currentUserId();
         $project_title = $request->get('project_title');
         $project = new Project(array(
             'title' => $project_title ,
             'user_id' =>$user_id,
         ));
         $project->save();
-       /*
-        * پروژه جدید در جدول projects ایجاد شده و همزمان باید رکورد ارتباط بین جدول user_project ایجاد شود
-        * در پایین این کار انجام شده است.
-        */
-        $latest_project = DB::table('projects')->where('user_id',$user_id)->latest('created_at')->first();
-        $user = User::find($user_id);
+        /*
 
-        $user->projects()->attach( $latest_project->id,['access'=>0]); // zero means owner access
+        */
+        $latest_project = $project->where('user_id',$user_id)->latest('created_at')->first();
+        $user->currentUser()->projects()->attach( $latest_project->id,['access'=>0]); // zero means owner access
         return redirect('/projects/index')->with('status', 'پروژه جدید ایجاد شد!');
     }
 

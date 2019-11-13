2b0e2df604699a2ea6a6ec5143c3055b5728dac4
shaho1090 <33988792+shaho1090@users.noreply.github.com>
2019-11-13 09:01:13 +0330

    correction

---
+++ app/Http/Controllers/WorkTimeProjectController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use App\UserWorkSpace;
+use App\WorkTime;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class WorkTimeProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Project $project_id
+     * @param WorkTime $workTime
+     * @return void
+     */
+    public function update(WorkTime $workTime, Project $project)
+    {
+        $workTime->update(['project_id' => $project->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
